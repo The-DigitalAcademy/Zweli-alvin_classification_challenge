4c0b74444b0581c468a05979c71cdcd884f8c0b4
Z-Khumalo <Zweli.Khumalo@dadevs.co.za>
2023-07-24 16:04:19 +0200

    deployed model

---
+++ purchas_clf.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.set_page_config(layout='wide',page_title='Address Book',page_icon='📚')
+
+st.title('Uber pickups in NYC')
+
+st.write('Select Model')
+selected_option = st.selectbox("Select an option", ["Random Forest Cl"])
+if selected_option == "Random Forest Cl":
+            model_load_path = "model.pkl"
+            with open(model_load_path, 'rb') as file:
+                model = pickle.load(file)
+                st.write("Machine learning Algorthm:", model)
